58e3ed3054af926d8858c90348e242ab068dd3a9
adprkh <50972471+adprkh@users.noreply.github.com>
2020-05-03 04:31:26 -0700

    Added Scrapers js file

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin","*") //disable for security on local
     res.header("Access-Control-Allow-Headers","Content-Type");
     next();
-})
+});
 
 
 app.get('/creators', async (req, res) => {
